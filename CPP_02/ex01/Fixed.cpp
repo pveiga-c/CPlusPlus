1316aae2fb1b6916538650283c43713e6b6dc87e
pveiga-c <correiapc@hotmail.com>
2024-03-26 10:12:20 +0000

    start cpp02/ex01

--- CPP_02/ex01/Fixed.cpp
+++ CPP_02/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: correia <correia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 11:29:03 by pveiga-c          #+#    #+#             */
-/*   Updated: 2024/03/25 17:11:00 by correia          ###   ########.fr       */
+/*   Updated: 2024/03/26 09:38:57 by correia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,31 @@
 
 	Fixed::Fixed()
 	{
+		this->m_fixedPoint = 0;
 		std::cout << "Default constructor called" << std::endl;
+	};
+	
+	Fixed::Fixed(const Fixed& copy)
+	{
+		std::cout << "Copy constructor called" << std::endl;
+		*this = copy;
+	};
+
+	Fixed::Fixed(const int intNum)
+	{
+		
+	};
+	
+	Fixed::Fixed(const float floatNum)
+	{
+		
+	};
+	
+	Fixed& Fixed::operator=( const Fixed &copy)
+	{
+		std::cout << "Copy assignment operator called" << std::endl;
+		this->m_fixedPoint = copy.getRawBits();
+		return(*this);
 	};
 	
 	Fixed::~Fixed()
@@ -22,3 +46,24 @@
 		std::cout << "Destructor called" << std::endl;
 	};
 	
+	int Fixed::getRawBits( void ) const
+	{
+		std::cout << "getRawBits member function called" << std::endl;
+		return (this->m_fixedPoint);
+	};
+	
+	void Fixed::setRawBits( int const raw )
+	{
+		this->m_fixedPoint = raw;
+	};
+
+	float Fixed::toFloat( void ) const
+	{
+		
+	};
+	
+	int Fixed::toInt( void ) const
+	{
+		
+	};
+
--- CPP_02/ex01/Fixed.hpp
+++ CPP_02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: correia <correia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 11:29:21 by pveiga-c          #+#    #+#             */
-/*   Updated: 2024/03/25 17:10:13 by correia          ###   ########.fr       */
+/*   Updated: 2024/03/26 09:39:41 by correia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,22 @@
 
 class Fixed
 {
-	private:
+private:
+	int m_fixedPoint;
+	static const int m_fractionalBits = 8;
 	
-	public:
+public:
+	Fixed();
+	Fixed(const Fixed& copy);
+	Fixed(const int intNum);
+	Fixed(const float floatNum);
+	Fixed &operator=( const Fixed &copy);
+	~Fixed();
+	
+	int		getRawBits( void ) const;
+	void	setRawBits( int const raw );
+	float	toFloat( void ) const;
+	int		toInt( void ) const;
 };
 
 
