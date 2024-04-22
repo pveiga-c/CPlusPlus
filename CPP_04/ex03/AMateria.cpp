10c9627ed39ae458c60db6026ff6d2cdf756e8c1
Pedro Cristovao Veiga Correia <pveiga-c@c2r10s2.42lisboa.com>
2024-04-22 18:57:26 +0100

    start cpp04/ex03

---
+++ CPP_04/ex03/AMateria.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AMateria.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pveiga-c <pveiga-c@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/22 18:50:58 by pveiga-c          #+#    #+#             */
+/*   Updated: 2024/04/22 18:56:17 by pveiga-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "AMateria.hpp"
+
+AMateria::AMateria()
+{
+	
+};
+AMateria::~AMateria()
+{
+	
+};
+AMateria::AMateria(const AMateria& copy)
+{
+	
+};
+AMateria& AMateria::operator=(const AMateria &copy)
+{
+	
+};
+
+AMateria::AMateria(std::string const& type)
+{
+	
+};
+
+std::string const & AMateria::getType() const
+{
+	
+};
---
+++ CPP_04/ex03/AMateria.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AMateria.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pveiga-c <pveiga-c@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/22 18:45:40 by pveiga-c          #+#    #+#             */
+/*   Updated: 2024/04/22 18:55:56 by pveiga-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef AMATERIA_HPP
+#define AMATERIA_HPP
+
+#include <iostream>
+
+class AMateria
+{
+	protected:
+
+	public:
+		AMateria();
+		~AMateria();
+		AMateria(const AMateria& copy);
+		AMateria& operator=(const AMateria &copy);
+		AMateria(std::string const& type);
+		
+		std::string const& getType() const; //Returns the materia type
+
+		virtual AMateria* clone() const = 0;
+		virtual void use(ICharacter& target);
+};
+
+#endif
